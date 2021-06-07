c81251d42b1a3aa9d7c216b545f13c4c8ca4b6f7
TebiV <estebannvalenti@gmail.com>
2021-06-07 02:30:36 -0300

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+//Motor de plantilla
+app.set('view engine','ejs');
+//Plantillas
+app.set('views', __dirname + '/views');
+
+
+
+app.use(express.static(__dirname + "/public"))
+
+
+
+app.get('/', (req,res)=>{
+    res.render("index", {titulo: "MI TITULO MEJOREADO A DINAMICO"})
+})
+
+app.get('/servicios', (req,res)=>{
+    res.render("servicios", {tituloServicios: "Mi subtitulo de servicios"})
+})
+
+app.use((req,res,next) => {
+    res.status(404).render("404", {
+        titulo404: "ERROR 404",
+        texto404: "ESTA PAGINA NO EXISTE PROBA EN OTRA"
+    })
+})
+
+app.listen(port, () => {
+    console.log('escuchando solicitudes en el puerto ', port)
+})
+
---
+++ frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['platano','manzana', 'pera', 'platano'];
+const dinero = 100;
+
+module.exports = {
+    frutas,
+    dinero
+}
