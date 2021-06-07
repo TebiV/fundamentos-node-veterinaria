2e90cb462c76911bf4ebfb9c8c9c5f6e1f750c03
TebiV <estebannvalenti@gmail.com>
2021-06-07 02:53:01 -0300

    Primer prueba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 //Motor de plantilla
 app.set('view engine','ejs');
 //Plantillas
@@ -11,15 +11,15 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + "/public"))
 
-
+app.get('/servicios', (req,res)=>{
+    res.render("servicios", {tituloServicios: "Mi subtitulo de servicios"})
+})
 
 app.get('/', (req,res)=>{
     res.render("index", {titulo: "MI TITULO MEJOREADO A DINAMICO"})
 })
 
-app.get('/servicios', (req,res)=>{
-    res.render("servicios", {tituloServicios: "Mi subtitulo de servicios"})
-})
+
 
 app.use((req,res,next) => {
     res.status(404).render("404", {
@@ -28,7 +28,10 @@ app.use((req,res,next) => {
     })
 })
 
-app.listen(port, () => {
-    console.log('escuchando solicitudes en el puerto ', port)
+app.listen(PORT, () => {
+    console.log('escuchando solicitudes en el puerto ', PORT)
 })
+
+
+
 
