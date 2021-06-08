9d379fcaa069463fab952852631d5ec5727e0b46
TebiV <estebannvalenti@gmail.com>
2021-06-08 01:59:32 -0300

    make it better

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.use((req,res,next) => {
         texto404: "ESTA PAGINA NO EXISTE PROBA EN OTRA"
     })
 })
-
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('escuchando solicitudes en el puerto ', PORT)
 })
 
